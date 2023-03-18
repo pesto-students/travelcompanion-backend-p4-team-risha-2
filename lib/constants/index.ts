97184727c607d0fde9d64ba542bd8ccdecb422d5
uma <varagamuma@gmail.com>
2023-03-18 08:43:45 +0530

    removed black slash for url

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,2 +1,2 @@
-export const CLIENT_URL = "https://travelcompaniondev.netlify.app/"
+export const CLIENT_URL = "https://travelcompaniondev.netlify.app"
 export const JWT_SECRET = "TRAVELCOMPANION_UMA_SHARYU";
